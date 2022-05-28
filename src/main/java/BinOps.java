b8a51b8cd9ec1eb62c64c7c1050944c1510f93f0
sirAfterGlow <shabalin.fallout@mail.ru>
2022-05-28 23:41:28 +0500

    Init commit

---
+++ src/main/java/BinOps.java
@@ -0,0 +1,21 @@
+public class BinOps {
+
+    public BinOps() {
+    }
+
+    public String sum(String a, String b) {
+        int arg1 = Integer.parseInt(a, 2);
+        int arg2 = Integer.parseInt(b, 2);
+        int result = arg1 + arg2;
+
+        return Integer.toBinaryString(result);
+    }
+
+    public String mult(String a, String b) {
+        int arg1 = Integer.parseInt(a, 2);
+        int arg2 = Integer.parseInt(b, 2);
+        int result = arg1 * arg2;
+
+        return Integer.toBinaryString(result);
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+
+        System.out.println(binOps.sum("01", "11"));
+        System.out.println(binOps.mult("10", "11"));
+    }
+}
